d29c00ee9eb2c43e21a61626a32fc5198c594f32
Nurşah DAĞLI <nursahdagli8@gmail.com>
2019-12-19 23:48:16 +0300

    Vektörlerde işlem, center ve distance hesaplama

--- 9.Hafta.py
+++ 9.Hafta.py
@@ -12,18 +12,20 @@ v_1,v_2
 # In[2]:
 
 
-v_1+v_2
+v_1+v_2  # iki vektörü birleştiriyor ( [1, 3, 2, -3] )
 
 
 # In[3]:
 
 
-def my_add(vector_1,vector_2):
+def my_add(vector_1,vector_2):     # uzunluğu bilinen
     vector_3=[0,0]
-    vector_3[0]=vector_1[0]+vector_2[0]
-    vector_3[1]=vector_1[1]+vector_2[1]
-    return vector_3
-def my_add_1(vec_1,vec_2):
+    vector_3[0]=vector_1[0]+vector_2[0] #ilk elemanları topluyor
+    vector_3[1]=vector_1[1]+vector_2[1] #ikinci elemanları topluyor
+    
+    return vector_3 # [3,0]
+
+def my_add_1(vec_1,vec_2):  #uzunluğu bilinmeyen
     
     s=len(vec_1)
     result_vec=[]
@@ -31,7 +33,9 @@ def my_add_1(vec_1,vec_2):
     for i in range (s):
         temp=vec_1[i]+vec_2[i]
         result_vec.append(temp)
-    return result_vec
+    
+    return result_vec # [3,0]
+
 import random
 def my_create_vectors(m=5,n=2):
     m,n=2,3 #iki tane 3 boyutlu liste
@@ -40,7 +44,7 @@ def my_create_vectors(m=5,n=2):
         my_vec.append([])
         for j in range(n):
             t=random.randint(-10,10)
-            my_vec[i].append(t)
+            my_vec[i].append(t) #ilk önce ilk vektörü daha sonra ikinci vektörü oluşturuyor.
             #pass
     return my_vec        
 
@@ -74,7 +78,7 @@ my_add_1(v_1,v_2)
 # In[8]:
 
 
-def my_center(vec_1,vec_2):
+def my_center(vec_1,vec_2): #vektörlerin her bir elemanları için ortalamalarını buluyor
     
     s=len(vec_1)
     result_vec=[]
@@ -96,7 +100,7 @@ my_center(v_1,v_2)
 # In[10]:
 
 
-def my_distance(v_1,v_2):
+def my_distance(v_1,v_2): #vektörler arası uzaklığı hesaplıyor
     s=len(v_1)
     t=0
     for i in range(s):
